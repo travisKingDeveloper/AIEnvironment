d20bad45783cc1d2be942b05ed30fffed7a2b77a
Travis <n00787683@ospreys.unf.edu>
2016-02-29 14:33:28 -0500

    Finallizing test code for the benefit of others involved in python development

---
+++ DecisionControl/MainExample.py
@@ -0,0 +1,23 @@
+from HighLevelHardwareCode.ExampleHighLevel import *
+from LowLevelHardwareCode.ExampleLowLevel import *
+__author__ = 'travi_000'
+
+
+print("Simulating Calling functions in different directory's")
+
+function_low_level()
+function_high_level()
+
+lowLevel = LowLevel()
+
+lowLevel.add_degree(20)
+
+lowLevel.add_degree(300)
+
+print("this is the degree " + str(lowLevel.wheelDegrees))
+
+lowLevel.add_degree(300)
+
+print("this is the degree " + str(lowLevel.wheelDegrees))
+
+print("I am concluding the test")
