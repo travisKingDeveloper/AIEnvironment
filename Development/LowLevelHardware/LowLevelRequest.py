187b31332399767b3c32df7409fb6ea98fd5f05d
${name} <${email}>
2016-03-07 18:39:04 -0500

    Created Low Level Requests and Responses

---
+++ Development/LowLevelHardware/LowLevelRequest.py
@@ -0,0 +1,10 @@
+__author__ = 'travi_000'
+
+
+class LowLevelRequest(object):
+    def __init__(self):
+        # constructor object
+        self.wheelDegrees = 0
+
+
+
---
+++ Development/LowLevelHardware/LowLevelResponse.py
@@ -0,0 +1,8 @@
+__author__ = 'travi_000'
+
+
+class LowLevelResponse(object):
+    def __init__(self):
+        # constructor object
+        self.wheelDegrees = 0
+
