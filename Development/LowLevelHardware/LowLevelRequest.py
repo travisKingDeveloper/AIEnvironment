267bc2fa64922b0921c8d23f44832a6ba38bf2a0
${name} <${email}>
2016-03-07 19:46:18 -0500

    Updated Low Level Requests and Responses

--- Development/LowLevelHardware/LowLevelRequest.py
+++ Development/LowLevelHardware/LowLevelRequest.py
@@ -4,13 +4,17 @@ __author__ = 'travi_000'
 
 
 class LowLevelRequest(object):
-    def __init__(self , WheelTurn, NumberOfDegrees, Movement, Servo, Gate):
+
+    def __init__(self , WheelTurn, NumberOfDegrees, Movement, Servo, Wall, CameraDegrees):
+        # Camera Degrees is 0 - 180
         # constructor object
         self.wheelTurn = WheelTurn
         self.movement = Movement
         self.servo = Servo
-        self.gate = Gate
+        # self.gate = Gate
         self.degrees = NumberOfDegrees
+        self.wall = Wall
+        self.CameraDegrees = CameraDegrees
 
     def giveMessage(self):
         returnVar = ""
@@ -24,7 +28,27 @@ class LowLevelRequest(object):
         returnVar += ("A" + "1" if self.servo == Servo.Down else "0" + "|")
         returnVar += "G"
 
+        # Commented Out For Further Use
+        # if self.gate == Gate.Left:
+        #     returnVar += "10|"
+        # elif self.gate == Gate.Right:
+        #     returnVar += "01|"
+        # else:
+        #     returnVar += "00|"
 
+        if self.wall != Wall.NoMovement:
+            returnVar += "W"
+            if self.wall == Wall.Close:
+                returnVar += "C|"
+            elif self.wall == Wall.Left:
+                returnVar += "L|"
+            else:
+                returnVar += "R|"
+
+        if self.CameraDegrees != -1:
+            returnVar += "C" + str(self.CameraDegrees)
+
+        return returnVar
 
 class WheelTurn(enum):
     Left = 1
@@ -43,7 +67,13 @@ class Servo(enum):
     Down = 1
 
 
-class Gate(enum):
-    Left = 10
-    Right = 01
-    Blocked = 00
+class Wall(enum):
+    Left = 1
+    Right = 2
+    NoMovement = 0
+    Close = 3
+
+# class Gate(enum):
+#     Left = 10
+#     Right = 01
+#     Blocked = 00
