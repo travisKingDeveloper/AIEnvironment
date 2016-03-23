051f7e2abe711249bce21501b2518e84a50d3a45
MichaelCDormann <michael.c.dormann@gmail.com>
2016-03-23 11:49:39 -0400

    All my changes

--- Development/LowLevelHardware/LowLevelResponse.py
+++ Development/LowLevelHardware/LowLevelResponse.py
@@ -12,14 +12,16 @@ class LowLevelResponse(object):
         self.isLineRight = False
 
     def getMessage(self, port):
-        line = port.read_until("\n")
+        line = port.readLine()
+        packets = line.split("\n")
 
-        if line[0] == "B":
-            self.isBumpedUpperLeft = (True if line[1] == 1 else False)
-            self.isBumpedUpperRight = (True if line[2] == 1 else False)
-            self.isBumpedLowerLeft = (True if line[3] == 1 else False)
-            self.isBumpedLowerRight = (True if line[4] == 1 else False)
-        elif line[0] == "L":
-            self.isLineLeft = (True if line[1] == 1 else False)
-            self.isLineCenter = (True if line[2] == 1 else False)
-            self.isLineRight = (True if line[1] == 1 else False)
+        for packet in packets:
+            if packet[0] == "B":
+                self.isBumpedUpperLeft = (True if packet[1] == 1 else False)
+                self.isBumpedUpperRight = (True if packet[2] == 1 else False)
+                self.isBumpedLowerLeft = (True if packet[3] == 1 else False)
+                self.isBumpedLowerRight = (True if packet[4] == 1 else False)
+            elif packet[0] == "L":
+                self.isLineLeft = (True if packet[1] == 1 else False)
+                self.isLineCenter = (True if packet[2] == 1 else False)
+                self.isLineRight = (True if packet[1] == 1 else False)
